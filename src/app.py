ee6448d4379a585415f971dc3487ea4add365716
Aly Elbana <alyelbana0@gmail.com>
2025-11-02 12:31:35 +0200

    setup finished

--- src/app.py
+++ src/app.py
@@ -1,40 +1,19 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from src.schema import Post
 from src.db import PostSchema, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    await create_db_and_tables()
+    yield
 
-text_posts = {
-    1: {"title": "Intro to FastAPI", "content": "FastAPI makes backend development super fast and clean."},
-    2: {"title": "Python Tips", "content": "Use list comprehensions for cleaner and faster Python code."},
-    3: {"title": "C++ for AI", "content": "Leverage C++ performance with modern AI integrations."},
-    4: {"title": "React & Node", "content": "Full-stack magic happens when React meets Node.js."},
-    5: {"title": "Data Cleaning 101", "content": "Good machine learning starts with good data preprocessing."},
-    6: {"title": "Weekend Motivation", "content": "Sometimes, a coffee and a walk solve more bugs than hours of debugging."},
-    7: {"title": "Deep Learning", "content": "Neural networks are powerful but remember: garbage in, garbage out."},
-    8: {"title": "API Design", "content": "Always document your API endpoints — future you will thank you."},
-    9: {"title": "Robotics & AI", "content": "When hardware meets intelligence, the future begins."},
-    10: {"title": "Career Advice", "content": "Build projects that show your skills, not just your knowledge."}
-}
+app = FastAPI(lifespan=lifespan)
 
-@app.get("/posts")
-async def get_all_posts(limit: int = None):
-        
-    if limit:
-        if limit > len(text_posts):
-            return text_posts
-        return list(text_posts.values())[:limit]
-    
-    return text_posts
 
-@app.get("/posts/{id}")
-async def get_post(id: int)->Post:
-    if id not in text_posts:
-        raise HTTPException(status_code=404, detail="Post Not Found")
-    return text_posts.get(id)
-    
-@app.post("/posts")
-async def create_post(post: Post)->Post:
-    new_post = {"title": post.title, "content": post.content}
-    text_posts[max(text_posts.keys()) + 1] = new_post
-    return new_post
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...),
+                      caption: str = Form(...),
+                      session: AsyncSession = Depends(get_async_session)):
+    pass
--- src/db.py
+++ src/db.py
@@ -16,7 +16,6 @@ class PostSchema(Base):
     __tablename__ = "posts"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("user.id"), nullable=False)
     caption = Column(Text)
     url = Column(String, nullable=False)
     file_type = Column(String, nullable=False)
@@ -28,7 +27,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
         
         
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
